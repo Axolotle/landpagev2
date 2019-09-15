3ecdc1b27aeac6c58ac03b0db212a149265ddb1e
Axolotle <nchesnais@mailoo.org>
2019-09-15 03:23:24 +0200

    refactored generation & add translation & alter markdown

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 import jinja2
-from markdown import markdown
+import re
+from markdown import markdown as mdRenderer
 from ruamel.yaml import YAML
 
-yaml=YAML(typ='safe')
 
-def markdownFilter(input):
-    return markdown(input)
+yaml = YAML(typ='safe')
+
+
+def markdown(text):
+    p = re.compile('(!\[[^]]*]\()')
+    return mdRenderer(p.sub(r'\1../medias/', text))
 
 def getYaml(path):
     with open(path) as f:
@@ -18,10 +22,11 @@ def getTemplates():
             return True
 
     templateLoader = jinja2.FileSystemLoader(searchpath='templates/')
-    templateEnv = jinja2.Environment(loader=templateLoader)
-    templateEnv.filters['markdown'] = markdownFilter
+    templateEnv = jinja2.Environment(loader=templateLoader, extensions=['jinja2.ext.i18n'])
+    # add markdown filter
+    templateEnv.filters['markdown'] = markdown
     return {
-        template.split('.', 1)[0] + '.md': templateEnv.get_template(template)
+        template.split('.', 1)[0] : templateEnv.get_template(template)
         for template in templateEnv.list_templates(filter_func=filterTemplates)
     }
 
@@ -37,28 +42,64 @@ def readPage(root, fn):
                 page['boxes'].append(markdown(content))
             elif content != '':
                 page['content' if _type is '' else _type] = markdown(content)
-    page['data']['uri'] = '/' + os.path.dirname(root) + '/'
-    page['data']['lang'] = os.path.basename(root)
     return page
 
-def main():
-    templates = getTemplates()
-    site = getYaml('site.yaml')
-    projects = {lang: [] for lang in site['langs']}
-    for root, dirs, files in os.walk('landpage'):
-        for fn in files:
-            if fn.endswith('.md') and fn != 'home.md':
-                page = readPage(root, fn)
-                if page:
-                    print('Rendering {}/{}…'.format(root, fn))
-                    templates[fn].stream(site=site, page=page).dump(os.path.join(root, 'index.html'))
-                    if fn == 'project.md':
-                        projects[page['data']['lang']].append(page['data'])
+def getPages(config):
+    pages = {lang: {'home': None, 'pages': []} for lang in config['langs']}
+    common = None
+    for root, dirs, files in os.walk(config['pages']):
+        for dir in dirs:
+            if dir in config['forbiddenFolders']:
+                dirs.remove(dir)
+        if 'common.yaml' in files:
+            common = getYaml(os.path.join(root, 'common.yaml'))
+        for file in files:
+            if file.endswith('.md'):
+                page = readPage(root, file)
+                data = page['data']
+                if common is not None:
+                    page['data'] = {**common, **data}
+                [page['data']['template'], page['data']['lang'], _] = file.split('.')
+                page['data']['uri'] = root + '/'
+                if page['data']['template'] == 'home':
+                    pages[page['data']['lang']]['home'] = page
+                else:
+                    pages[page['data']['lang']]['pages'].append(page)
+                # create folders 'fr' and 'en' if they don't exist
+                for lang in config['langs']:
+                    os.makedirs(root + '/' + lang, exist_ok=True)
+        common = None
+    return pages
 
-    for lang, list in projects.items():
-        print('Rendering landpage/{}/home.md…'.format(lang))
-        page = readPage('landpage/'+lang, 'home.md')
-        templates['home.md'].stream(site=site, page=page, projects=list).dump('landpage/{}/index.html'.format(lang))
+def generate():
+    templates = getTemplates()
+    trad = getYaml('languages.yaml')
+    site = getYaml('site.yaml')
+    pages = getPages(site)
+
+    for lang, content in pages.items():
+        site['footer'] = content['home']['footer']
+        site['meta'] = content['home']['data']['meta']
+        projects = []
+        for page in content['pages']:
+            data = page['data']
+            if data['template'] == 'project':
+                projects.append(data)
+            print('Rendering {}…'.format(os.path.join(data['uri'], lang)))
+            templates[data['template']].stream(
+                site=site,
+                page=page,
+                trad=trad[lang],
+            ).dump('{}/{}/index.html'.format(data['uri'], data['lang']))
+
+        print('Rendering {}…'.format(os.path.join(site['pages'], lang)))
+        templates['home'].stream(
+            site=site,
+            page=content['home'],
+            projects=projects,
+            trad=trad[lang],
+        ).dump('{}{}/index.html'.format(content['home']['data']['uri'], lang))
+
 
 if __name__ == '__main__':
-    main()
+    generate()
